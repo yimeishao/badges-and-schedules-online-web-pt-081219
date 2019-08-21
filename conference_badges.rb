c6425a3d7a23c2311f7fd6151a1f8a73e5d284c7
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-21 20:41:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,8 +19,6 @@ def assign_rooms(speakers)
 end
 
 def printer(attendees)
- batch_badge_creator(attendees).each do |badge|
-   badge
- assign_rooms(attendees).each do |rooms|
-   rooms
+ batch_badge_creator(attendees).each{|badge| puts badge}
+ assign_rooms(attendees).each{|room| puts room}
  end
