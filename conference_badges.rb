690f9f6ee29a40ea036c590289a7a26ce40b99a3
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-21 20:32:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,6 +19,6 @@ def assign_rooms(speakers)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each do |speaker|
+  batch_badge_creator(attendees).each do |badge|
     puts badge
     
